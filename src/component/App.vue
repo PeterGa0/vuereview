93d760d92b237ac9bf50a8b49132c70c79c60edc
PeterGao <757789447@qq.com>
2018-04-25 14:06:09 +0800

    集成完毕

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         test
+        <router-view></router-view>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+import 'normalize.css';
 import Vue from 'vue';
-// import VueRouter from 'vue-router';
-// import elementUI from 'element-ui';
-// import axios from 'axios';
-
+import VueRouter from 'vue-router';
+import elementUI, { Aside } from 'element-ui';
+import axios from 'axios';
+Vue.prototype.$http=axios;
+Vue.use(VueRouter);
 import App from './component/App.vue';
+import routerConfig from'./router/routerConfig.js';
+
+
+import api from'./js/apiConfig.js';
+
+Vue.prototype.$api=api;
+
+axios.defaults.baseURL = 'http://157.122.54.189:9095';
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(elementUI);
 new Vue({
     el:"#app",
-    render: c=>c(App)
+    render: c=>c(App),
+    router:new VueRouter(routerConfig)
 })
