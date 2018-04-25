eec7ede7dcecbb437f92d4f9257e7f5407d6ff44
PeterGao <757789447@qq.com>
2018-04-25 13:23:26 +0800

    环境测试

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+// import VueRouter from 'vue-router';
+// import elementUI from 'element-ui';
+// import axios from 'axios';
+
+import App from './component/App.vue';
+new Vue({
+    el:"#app",
+    render: c=>c(App)
+})
